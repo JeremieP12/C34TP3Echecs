95ef42dae11a561e1636c90120adcc2a842dc8a2
JeremieP12 <djepro4@gmail.com>
2025-05-15 12:29:51 -0400

    Chemin possible à faire

--- src/projet_echecs/Echiquier.java
+++ src/projet_echecs/Echiquier.java
@@ -14,6 +14,27 @@ public class Echiquier implements MethodesEchiquier{
 
     @Override
     public void debuter() {
+        this.getCase(0,0).setPieceSurCase(new Tour("t1","noir"));
+        this.getCase(0,1).setPieceSurCase(new Cavalier("c1","noir"));
+        this.getCase(0,2).setPieceSurCase(new Fou("f1","noir"));
+        this.getCase(0,3).setPieceSurCase(new Reine("r1","noir"));
+        this.getCase(0,4).setPieceSurCase(new Roi("noir"));
+        this.getCase(0,5).setPieceSurCase(new Fou("f2","noir"));
+        this.getCase(0,6).setPieceSurCase(new Cavalier("c2","noir"));
+        this.getCase(0,7).setPieceSurCase(new Tour("t2","noir"));
+        this.getCase(7,0).setPieceSurCase(new Tour("t1","blanc"));
+        this.getCase(7,1).setPieceSurCase(new Cavalier("c1","blanc"));
+        this.getCase(7,2).setPieceSurCase(new Fou("f1","blanc"));
+        this.getCase(7,3).setPieceSurCase(new Reine("r1","blanc"));
+        this.getCase(7,4).setPieceSurCase(new Roi("blanc"));
+        this.getCase(7,5).setPieceSurCase(new Fou("f2","blanc"));
+        this.getCase(7,6).setPieceSurCase(new Cavalier("c2","blanc"));
+        this.getCase(7,7).setPieceSurCase(new Tour("t2","blanc"));
+
+        for (int i = 0; i < 8; i++) {
+            this.getCase(1,i).setPieceSurCase(new Pion("p"+i,"noir"));
+            this.getCase(6,i).setPieceSurCase(new Pion("p"+i,"blanc"));
+        }
 
     }
 
@@ -24,7 +45,9 @@ public class Echiquier implements MethodesEchiquier{
 
     @Override
     public boolean cheminPossible(Position depart, Position arrivee) {
-        return false;
+        if (!this.getCase(depart.getLigne(),depart.getColonne()).getPieceSurCase().estValide(arrivee,arrivee)){
+            return false;
+        }
     }
 
     @Override
